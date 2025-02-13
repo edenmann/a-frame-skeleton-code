37e0d430cad94940d309490de1b2a4c0003c9659
Eden Bashan <edenprofessional@outlook.com>
2025-02-13 17:24:51 +0000

    scene changing button

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function switchScene() {
+    const scene1 = document.getElementById("scene1")
+    const scene2 = document.getElementById("scene2")
 
+    if (scene1.style.display !== 'none') {
+        scene1.style.display = 'none';
+        scene2.style.display = 'block';
+    }
+
+    else {
+        scene1.style.display = 'block';
+        scene2.style.display = 'none';
+    }
+
+}
